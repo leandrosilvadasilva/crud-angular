ebe94c2e1518032efacd80b9a322b7da54b7efbe
leandro-silva <leandro.tecnicoinfo@gmail.com>
2024-09-09 06:39:54 -0300

    primeiro commit

---
+++ src/app/courses/model/course.ts
@@ -0,0 +1,9 @@
+export interface Course {
+
+  _id:      string;
+  name:     string;
+  category: string;
+
+}
+
+
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import{ HttpClient } from '@angular/common/http';
+
+import { Course } from '../model/course';
+import { delay, first, take, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list(){
+
+    return  this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(5),
+      tap(courses => console.log(courses))
+
+
+    );
+
+  }
+}
